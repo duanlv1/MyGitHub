6ab118ad0bc1f26b2227d2a12f34f7232f90cabe
changhong <changhong@baidu.com>
2018-07-28 12:52:09 +0800

    HELLO WORLD

--- WpfApp1/myproject.bat
+++ WpfApp1/myproject.bat
@@ -1,3 +1,3 @@
 set devnev="D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe"
 
-%devnev% "D:\Program Files (x86)\Jenkins\workspace\Test1\WpfApp1.sln" /rebuild
+%devnev% "D:\Program Files (x86)\Jenkins\workspace\Test1\WpfApp1\WpfApp1.sln" /rebuild
